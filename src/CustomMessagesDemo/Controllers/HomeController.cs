dbad912cd812d3cabfb0501b4823192cd82b81ab
frankabbruzzese <frankabbruzzese@gmail.com>
2017-09-14 20:25:03 +0200

    Aggiungere i file di progetto.

---
+++ src/CustomMessagesDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CustomMessagesDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMessagesDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(TestViewModel vm)
+        {
+            return View(vm);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/CustomMessagesDemo/ViewModels/TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomMessagesDemo.ViewModels
+{
+    public class TestViewModel
+    {
+        [Range(1, 10)]
+        public decimal ADecimal { get; set; }
+    }
+}
